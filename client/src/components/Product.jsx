543b4308b753651fdc60d9e2ba81af10149bd03d
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-04-16 17:13:56 +0100

    create product and product card file

---
+++ client/src/components/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+const Product = () => {
+  return (
+    <div className='py-10'>
+        <div className='flex flex-col items-center gap-4'>
+            <h1 className='text-2xl bg-black text-white py-2 px-8 lg:w-80 text-center'>Shopping Everyday</h1>
+            <span className='w-20 h-[3px] bg-black'></span>
+            <p className=' max-w-[700px] text-gray-600 text-center'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis veniam magnam dignissimos aliquid laudantium nihil eligendi nam delectus optio autem, ab dolores voluptatum excepturi quaerat tempore, mollitia fugiat,
+                nesciunt quod voluptatem. Accusamus neque quam fugiat, doloremque tempore obcaecati distinctio nisi?
+            </p>
+        </div>
+        <div className='max-w-screen-xl mx-auto'>
+            <ProductCard />
+        </div>
+    </div>
+  )
+}
+
+export default Product
